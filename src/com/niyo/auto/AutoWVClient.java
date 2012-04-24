34a3f691c0e23269fff7b4218a50edd547bcaa10
Ori Harel <ori.harel@gmail.com>
2012-04-24 15:18:57 +0300

    auto unite

---
+++ src/com/niyo/auto/AutoWVClient.java
@@ -0,0 +1,15 @@
+package com.niyo.auto;
+
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class AutoWVClient extends WebViewClient {
+
+	@Override
+	public void onPageFinished(WebView webView, String url) 
+	{
+		
+		
+	}
+	
+}
