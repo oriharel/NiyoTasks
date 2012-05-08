e1b205e994d02b6b7ffe8e76d66421a1ec2155ae
Ori Harel <ori.harel@gmail.com>
2012-05-08 22:44:23 +0300

    bt

--- src/com/niyo/auto/AppLauncher.java
+++ src/com/niyo/auto/AppLauncher.java
@@ -1,14 +1,20 @@
 package com.niyo.auto;
 
+import com.niyo.ClientLog;
+
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
 public class AppLauncher extends BroadcastReceiver {
 
+	private static final String LOG_TAG = AppLauncher.class.getSimpleName();
+	
 	@Override
 	public void onReceive(Context context, Intent intent) 
 	{
+		ClientLog.d(LOG_TAG, "recieved AppLauncher with "+intent.getAction());
+		
 		Intent intent2Launch = new Intent(context, AutoActivity.class);
 		intent2Launch.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 		context.startActivity(intent2Launch);
